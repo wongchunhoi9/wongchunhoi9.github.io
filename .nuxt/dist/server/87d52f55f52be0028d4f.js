be964fed2c67fb6ad6fe88c54207d57ea84ca175
wongchunhoi9 <57960522+wongchunhoi9@users.noreply.github.com>
2022-10-29 17:34:54 +0800

    update

---
+++ .nuxt/dist/server/87d52f55f52be0028d4f.js
@@ -0,0 +1,1 @@
+exports.ids=[22],exports.modules={554:function(e,t,r){"use strict";r.r(t);var l=r(1),component=Object(l.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,"4eb8edc8");t.default=component.exports}};
---
+++ .nuxt/dist/server/e74e1fa6790e519c0848.js
@@ -0,0 +1,1 @@
+exports.ids=[16],exports.modules={535:function(e,t,r){"use strict";r.r(t);var c={fetch:({redirect:e})=>e("https://peace-thepassenger.bandcamp.com/album/about-a-stalker")},n=r(1);var component=Object(n.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,(function(e){}),null,"c935b5a8");t.default=component.exports}};
